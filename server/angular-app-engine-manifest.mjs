202d15ed87fbf3493f6023b412b19b64cc2efb50
Mavish66 <mavishthota1314@gmail.com>
2025-03-14 00:58:24 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/mavish',
+  basePath: '/mavish-thota',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/mavish/',
+  baseHref: '/mavish-thota/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/mavish"
+    "route": "/mavish-thota"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11015, hash: 'a1c044ee775864ff715b7f1d754dd17fca23da8b5c95942782aea1deee99336f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1016, hash: '9279c20f887f75fcd5b0ba261da08482d93c454abfa7071d68994a37bf629413', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 35538, hash: '7dd3619d1747ac79431ea858a064659852ee8bb36d6184ad9f476158c7504845', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11021, hash: '0073641faa5eabfd2d260777e9cc3a7e1d0884bf12dc0cb248416f73dc16ef37', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1022, hash: '411a1a44cf15e1ef01dd63e70d561af70d4bb2afb477b17f4d699ade0f2654fb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 35544, hash: '8a15a6877a794cab6e82c5ad19d88900101a842a3d5594efaa5089fd1a746d00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-BPNBRDPQ.css': {size: 311436, hash: 'I/WxWCaGJZU', text: () => import('./assets-chunks/styles-BPNBRDPQ_css.mjs').then(m => m.default)}
   },
 };
